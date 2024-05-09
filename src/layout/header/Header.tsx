dd46cce1c1ff2361da235eb346d201ee9fcf3b04
AlbogachievM <albogachiev-2014@mail.ru>
2024-05-09 08:55:09 +0300

    first commit

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="" alt=""/>
+            <div className="header_panel">
+                <menu></menu>
+                <span>+38 (097) 987 78 88</span>
+                <div>
+                    <span>request for a call</span>
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </header>
+    );
+};
+
